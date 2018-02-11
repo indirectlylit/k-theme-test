da2dc14058c11165d9c599c4252b0e325f76ca06
Jamie Alexandre <jamalex@gmail.com>
2018-02-11 23:08:50 +0300

    Add ?level=<num>&root=<id> to manifest API, and permalink redirects

--- kolibri_un_women_plugin/ims/urls.py
+++ kolibri_un_women_plugin/ims/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^ccthin/manifest.xml$', views.ThinCommonCartridgeManifestView.as_view(), name='thinccmanifest'),
+    url(r'^permalink$', views.ContentPermalinkRedirect.as_view(), name='permalink'),
 ]
